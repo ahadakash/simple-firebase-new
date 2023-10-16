bb30225e6a296ff8bd0c477c650bfd64bc198444
ahadakash <sadhinakash181@gmail.com>
2023-10-16 23:57:17 +0600

    first commit

---
+++ src/__steps.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 1. visit: console.firebase.google.com 
+ * 2. create project (skip google analytics)
+ * 3. Register app (create config)
+ * 4. install firebase: npm install firebase
+ * 5. add config file to your project
+ * 6. DANGER do not publish or make firebase config to public by pushing those to github
+ * 7. Then router setup in the main.jsx file from react router tutorial site (Browser router) (Add 3 things)
+ * 8. Create teo folder in src > Layout & Components > 
+ * Layout > Main.jsx , Components > Header.jsx 
+ * Add Header and Layout in Main.jsx such as > <div> <Header/> <layout/> </div>
+ * 9. 
+ * 
+* */
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAomD5crJIlGDiksf8Fx-ff3a_lJLHVEEY",
+    authDomain: "simple-firebase-new1.firebaseapp.com",
+    projectId: "simple-firebase-new1",
+    storageBucket: "simple-firebase-new1.appspot.com",
+    messagingSenderId: "1042723772709",
+    appId: "1:1042723772709:web:576fbb835cf9b3af3322cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
